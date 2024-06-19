6d9d02d0142c970aaba19329c54e2d45144a8a8f
Muhammad Rafi Munggaran <ethanjeger123@gmail.com>
2024-06-19 20:43:11 +0700

    new

---
+++ Pertemuan 11/entrybutton.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+print('=============== Mencetak dadta dari Entry Widget dengan Button')
+
+root = Tk()
+root.geometry('400x200')
+
+def CetakData():
+    teks = entry1.get()
+    print(teks)
+    return None
+
+entry1 = Entry(root,width = 20)
+entry1.pack()
+
+Button(root, text = "cetak data",  command = CetakData).pack()
+
+root.mainloop()
